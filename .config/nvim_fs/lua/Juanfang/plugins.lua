5232160c17deb0e43da34c15044d61c7f42a4eca
Juan P Barrios <jbarrios@gmail.com>
2023-05-19 15:12:13 +0200

    add barbar topline plugin

--- .config/nvim_fs/lua/Juanfang/plugins.lua
+++ .config/nvim_fs/lua/Juanfang/plugins.lua
@@ -102,6 +102,14 @@ return packer.startup(function(use)
 
   use "lewis6991/gitsigns.nvim"
 
+  use {
+    "romgrk/barbar.nvim",
+    requires = {
+      "lewis6991/gitsigns.nvim",
+      "nvim-tree/nvim-web-devicons"
+    }
+  }
+
   -- Automoatically set up configuration after cloning packer.nvim
   -- should be the last command after all plugins
   if PACKER_BOOTSTRAP then
