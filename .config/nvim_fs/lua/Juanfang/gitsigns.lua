f278e45b22a6d37cc4de3db364cca7c73f27fcfd
Juan P Barrios <jbarrios@gmail.com>
2023-05-23 11:10:09 +0200

    add map descriptions. restyle

--- .config/nvim_fs/lua/Juanfang/gitsigns.lua
+++ .config/nvim_fs/lua/Juanfang/gitsigns.lua
@@ -1,9 +1,7 @@
 local status_ok, gitsigns = pcall(require, "gitsigns")
-if not status_ok then
-  return
-end
+if not status_ok then return end
 
-gitsigns.setup {
+gitsigns.setup({
   signs = {
     add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
     change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -42,46 +40,53 @@ gitsigns.setup {
     col = 1,
   },
   yadm = {
-    enable = false
+    enable = false,
   },
   on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
+    local gs = package.loaded.gitsigns
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-
-      -- Navigation
-      map('n', ']c', function()
-        if vim.wo.diff then return ']c' end
-        vim.schedule(function() gs.next_hunk() end)
-        return '<Ignore>'
-      end, {expr=true})
-
-      map('n', '[c', function()
-        if vim.wo.diff then return '[c' end
-        vim.schedule(function() gs.prev_hunk() end)
-        return '<Ignore>'
-      end, {expr=true})
-
-      -- Actions
-      map('n', '<leader>hs', gs.stage_hunk)
-      map('n', '<leader>hr', gs.reset_hunk)
-      map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-      map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-      map('n', '<leader>hS', gs.stage_buffer)
-      map('n', '<leader>hu', gs.undo_stage_hunk)
-      map('n', '<leader>hR', gs.reset_buffer)
-      map('n', '<leader>hp', gs.preview_hunk)
-      map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-      map('n', '<leader>tb', gs.toggle_current_line_blame)
-      map('n', '<leader>hd', gs.diffthis)
-      map('n', '<leader>hD', function() gs.diffthis('~') end)
-      map('n', '<leader>td', gs.toggle_deleted)
-
-      -- Text object
-      map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
     end
-}
+
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then return "]c" end
+
+      vim.schedule(function() gs.next_hunk() end)
+
+      return "<Ignore>"
+    end, { expr = true, desc = "git next hunk" })
+
+    map("n", "[c", function()
+      if vim.wo.diff then return "[c" end
+
+      vim.schedule(function() gs.prev_hunk() end)
+
+      return "<Ignore>"
+    end, { expr = true, desc = "git prev hunk" })
+
+    -- Actions
+    map("n", "<leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
+    map("n", "<leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
+    map("n", "<leader>hS", gs.stage_buffer, { desc = "git stage buffer" })
+    map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "git undo stage hunk" })
+    map("n", "<leader>hR", gs.reset_buffer, { desc = "git reset buffer" })
+    map("n", "<leader>hp", gs.preview_hunk, { desc = "git preview hunk" })
+    map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, { desc = "git full blame line" })
+    map("n", "<leader>hd", gs.diffthis, { desc = "git diff this" })
+    map("n", "<leader>hD", function() gs.diffthis("~") end, { desc = "git diff this ~" })
+
+    map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "git toggle current line blame" })
+    map("n", "<leader>td", gs.toggle_deleted, { desc = "git toggle deleted" })
+
+    local v_args = { vim.fn.line("."), vim.fn.line("v") }
+    map("v", "<leader>hs", function() gs.stage_hunk(v_args) end, { desc = "git stage hunk" })
+    map("v", "<leader>hr", function() gs.reset_hunk(v_args) end, { desc = "git reset hunk" })
+
+    -- Text object
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+  end,
+})
